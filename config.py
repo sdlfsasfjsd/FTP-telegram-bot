bfb7a3b7758f0594c8f18104d926445cb034008d
sdlfsasfjsd <maincode1@proton.me>
2025-04-02 18:47:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,21 +1,21 @@
 import os
 
 #API ID & API Hash -----> my.telegram.org
-api_id = 0000000 
-api_hash = 'e28cc5d85c66rr785422fe3bf50e61f'
+api_id = 8002628
+api_hash = '0805d45de882197f254fd11b3105409a'
 
 #Bot Token -----> @BotFather
-token = '8569471455:JJkiyA6MmuB570F4t5yVLmi92tWRqQi6rty'
+token = '7212730524:AAHxOd47ZxyVwuMZA8ce9N4CreDoH1wXnSA'
 
 #Session Name -----> optional
 session_name = 'FTP_Manager'
 
 
 #The robot admin (the person who can give orders to the robot.) -----> @myidbot
-admins = [1117638015,1117638015,1117638015]
+admins = [1133933758]
 
 # Chat id to send technical logs
-dev = 1117638015
+dev = -1001593556762
 
 # When a file is sent to the bot, first that file is downloaded from the Telegram repository and stored in the bot's server.
 # Here you need to specify its temporary storage path
@@ -24,11 +24,11 @@ dl_path = os.path.abspath(os.getcwd()) + '/'
 
 
 # The upload path where we give FTP access to the bot.
-ftp_path = '/public_html/Bot/'
+ftp_path = '/home/bicvgast/madflixes.xyz/madflix2x7'
 
 # The files are temporarily downloaded after they are on the bot server. They are uploaded to another host through FTP.
 # Here we have to give FTP access to the bot.
-ftp_ip = '95.136.117.620'
-ftp_username = 'FTP@domain.com'
-ftp_password = '12345678'
-ftp_domain = 'https://domain.com/Bot/'
+ftp_ip = '37.27.108.55'
+ftp_username = 'madflix2x7@madflixes.xyz'
+ftp_password = '@Yasir24x7'
+ftp_domain = 'ftp.madflixes.xyz'
